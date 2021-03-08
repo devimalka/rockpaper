37fdbf137491a9e06a6807626d645d635c6276f0
devim <developerimalka@gmail.com>
2021-03-08 11:04:49 +0530

    completed the game but not the winner select

--- script.js
+++ script.js
@@ -4,32 +4,77 @@ function computerPlay(){
     return randomchoose;
     
  }
-
-
+let playerScore=0;
+let computerScore=0;
+function displayResults(text,computer,human){
+    let com = document.querySelector("#com");
+    let hum = document.querySelector("#hum");
+    let results = document.querySelector('.results');
+    let li = document.createElement('li');
+    li.textContent=text;
+    results.appendChild(li);
+    com.textContent=computer;
+    hum.textContent=human;
+    
 }
+ 
 
 function playRound(playerSelection, ComputerSelection){
+    if(playerScore===5){
+        
+        playerScore=0;
+        computerScore=0;
+        displayResults("winner is player",computerScore,playerScore);
+    }
+    else if(computerScore===5){
+       
+        playerScore=0;
+        computerScore=0;
+        displayResults("winner is computer",computerScore,playerScore);
+    }
+  
+
     if(playerSelection=="rock" && ComputerSelection=="paper"){
-        displayResluts("you loose paper beats rock");
+        computerScore++;
+        displayResults("computer wins paper beats rock",computerScore,playerScore); 
     }
     else if(playerSelection == "paper" && ComputerSelection == "rock"){
-        displayResluts("you win paper beats rock");
+        playerScore++;
+        displayResults("You win paper beats rock",computerScore,playerScore);
+
     }
     else if(playerSelection == "scissor" && ComputerSelection == "paper"){
-        displayResluts("you win scissor cuts paper");
+        playerScore++;
+        displayResults("You win scissor cuts paper",computerScore,playerScore);
     }
     else if(playerSelection == "rock" && ComputerSelection == "scissor"){
-        displayResluts("you win rock beats scissor");
+        playerScore++;
+        displayResults("you win rock beats scissor",computerScore,playerScore);
+       
+
     }
     else if(playerSelection == "paper" && ComputerSelection == "scissor"){
-        displayResluts("you loose scissor cuts paper");
+        computerScore++;
+        displayResults("computer wins scissor cuts paper",computerScore,playerScore);
+        
     }
     else if(playerSelection == "scissor" && ComputerSelection == "rock"){
-        displayResluts("you loose rock beats scissor");
+        computerScore++;
+        displayResults("computer wins rock beats scissor",computerScore,playerScore);
+       
     }
     else {
-        displayResluts("tied");
+        displayResults("tied",computerScore,playerScore);
     }
+
+    
 }
 
+
+
+const buttons = document.querySelectorAll('.human button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click',() => playRound(button.id,computerPlay()));
+})
 
